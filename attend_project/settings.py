79db53be5296dbec693d112d73f6e1dd0f5e5c10
Grace IR <graceonyenaturuchi11@gmail.com>
2026-03-23 16:30:03 +0100

    Fix ALLOWED_HOSTS for Vercel

--- attend_project/settings.py
+++ attend_project/settings.py
@@ -23,10 +23,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c7&k6mv4x=k&wo#h6-vl6)u$plz#y)-p4v(cn08fnvl&xead%d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.vercel.app',      # matches any vercel.app subdomain
+    'localhost',
+    '127.0.0.1',
+]
 
+USE_X_FORWARDED_HOST = True
+
+# Optional: Trust the X-Forwarded-Proto header to handle HTTPS
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
